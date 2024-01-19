8d8566cd65306227311308e6574945854a356569
kwonbumjoon <89958988+kwonbumjoon@users.noreply.github.com>
2024-01-19 15:43:27 +0900

    Add files via upload

---
+++ BookMarket/src/Welcome.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+//		System.out.println("Welcome to Shopping Mall");
+//		System.out.println("Welcome to Book Market!");
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("당신의 이름을 입력하세요 : ");
+		String userName = input.next();
+		
+		System.out.print("연락처를 입력하세요 : ");
+		int userMobile = input.nextInt();
+		
+		
+		String greeting = "Welcome to Shopping Mall";
+		String tagline = "Welcome to Book Market!";
+		
+		System.out.println("****************************************************");
+		System.out.println("\t"+greeting);
+		System.out.println("\t"+tagline);
+		System.out.println("****************************************************");
+		
+		System.out.println("1. 고객 정보 확인하기 \t4. 바구이네 항목 추가하기");
+		System.out.println("2. 장바구니 상품 목록 보기 \t5. 장바구니의 항목 수량 줄이기");
+		System.out.println("3. 장바구니 비우기 \t\t6. 장바구니에 항목 삭제하기");
+		System.out.println("7. 영수증 표기하기 \t\t8. 종료");
+		System.out.println("****************************************************");
+		System.out.print("메뉴 번호를 선택해주세요 ");
+		int n = input.nextInt();
+		System.out.println(n + "번을 선택하셨습니다.");
+	
+	}
+
+}
---
+++ Chap04/src/src/Example01.java
@@ -0,0 +1,18 @@
+package src;
+
+public class Example01 {
+
+	public static void main(String[] args) {
+
+		int num = 10;
+		
+		if(num > 15)
+		{
+			System.out.println("숫자가 15보다 큽니다.");
+		}
+		
+		System.out.println("이 문장은 if문 외부에 있습니다.");
+
+	}
+
+}
